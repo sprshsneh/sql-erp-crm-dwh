e6e4ddbd3e654ad23f5233690d6a6ce08a66dfb5
Sneh Sparsh <98311517+sprshsneh@users.noreply.github.com>
2025-11-16 00:37:41 +0530

    Create data_load_check.sql

---
+++ scripts/Others/data_load_check.sql
@@ -0,0 +1,9 @@
+SELECT TOP 10 * FROM [CRM_ERP_DWH].[silver].[crm_cust_info] ;
+SELECT TOP 10 * FROM [CRM_ERP_DWH].[silver].[crm_prd_info] ;
+SELECT TOP 10 * FROM [CRM_ERP_DWH].[silver].[crm_sales_details];
+SELECT TOP 10 * FROM [CRM_ERP_DWH].[silver].[erp_cust_az12];
+SELECT TOP 10 * FROM [CRM_ERP_DWH].[silver].[erp_loc_a101];
+SELECT TOP 10 * FROM [CRM_ERP_DWH].[silver].[erp_px_cat_g1v2];
+SELECT TOP 10 * FROM [CRM_ERP_DWH].[silver].[etl_audit_log]
+
+
